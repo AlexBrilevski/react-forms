415b1e8d8b915b2fa585a1c0baefc38390afd751
Alex Brilevski <brilevski.alex@gmail.com>
2025-11-13 21:04:48 +0300

    Email validation on keystroke was added to Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,8 @@ export default function Login() {
     password: '',
   });
 
+  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
@@ -43,6 +45,7 @@ export default function Login() {
             name="email"
             value={enteredValues.email}
             onChange={(e) => handleValueCnage('email', e.target.value)} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
