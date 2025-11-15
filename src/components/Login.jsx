0e95f555848a40f15ffe56842fc8bcbadd5efa40
Alex Brilevski <brilevski.alex@gmail.com>
2025-11-15 17:03:21 +0300

    Logic in the Login component was refactored with the custom useInput hook

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,38 +1,37 @@
 import { useState } from "react";
+import { useInput } from "../hooks/useInput.js";
 import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
 import Input from "./Input.jsx";
 
 export default function Login() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: '',
-  });
+  const {
+    value: email,
+    handleChange: handleEmailChange,
+    handleBlur: handlrEmailBlur,
+    resetValue: resetEmail,
+    hasError: emailIsInvalid,
+  } = useInput('', isEmail);
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid = didEdit.email && !isEmail(enteredValues.email);
-  const passwordIsInvalid = didEdit.password && !hasMinLength(enteredValues.password, 6);
+  const {
+    value: password,
+    handleChange: handlePasswordChange,
+    handleBlur: handlrPasswordBlur,
+    resetValue: resetPassword,
+    hasError: passwordIsInvalid,
+  } = useInput('', (value) => hasMinLength(value, 6));
 
   function resetForm() {
-    setEnteredValues({
-      email: '',
-      password: '',
-    });
-    setDidEdit({
-      email: false,
-      password: false,
-    });
+    console.log('Form data reset');
+    resetEmail();
+    resetPassword();
   }
 
   function handleSubmit(event) {
     event.preventDefault();
 
     if (
-      !isNotEmpty(enteredValues.email) ||
-      !isNotEmpty(enteredValues.password) ||
+      !isNotEmpty(email) ||
+      !isNotEmpty(password) ||
       emailIsInvalid ||
       passwordIsInvalid
     ) {
@@ -40,32 +39,13 @@ export default function Login() {
       return;
     }
 
-    console.log(enteredValues);
+    console.log({ email, password });
     resetForm();
   }
 
   function handleFormReset(event) {
     event.preventDefault();
     resetForm();
-  }
-
-  function handleValueCnage(fieldId, value) {
-    setEnteredValues(prevState => ({
-      ...prevState,
-      [fieldId]: value,
-    }));
-
-    setDidEdit(prevState => ({
-      ...prevState,
-      [fieldId]: false,
-    }));
-  }
-
-  function handleInputBlur(fieldId) {
-    setDidEdit(prevState => ({
-      ...prevState,
-      [fieldId]: true,
-    }));
   }
 
   return (
@@ -77,9 +57,9 @@ export default function Login() {
           label={'Email'}
           id={'email'}
           name={'email'}
-          value={enteredValues.email}
-          onChange={(e) => handleValueCnage('email', e.target.value)}
-          onBlur={() => handleInputBlur('email')}
+          value={email}
+          onChange={handleEmailChange}
+          onBlur={handlrEmailBlur}
           error={emailIsInvalid && 'Please enter a valid email address.'}
         />
 
@@ -88,9 +68,9 @@ export default function Login() {
           id={'password'}
           name={'password'}
           type={'password'}
-          value={enteredValues.password}
-          onChange={(e) => handleValueCnage('password', e.target.value)}
-          onBlur={() => handleInputBlur('password')}
+          value={password}
+          onChange={handlePasswordChange}
+          onBlur={handlrPasswordBlur}
           error={passwordIsInvalid && 'Please enter a valid password.'}
         />
       </div>
